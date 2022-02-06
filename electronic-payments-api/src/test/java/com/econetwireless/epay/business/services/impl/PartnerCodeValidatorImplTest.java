1dae37d0727d2100f7ba2f20146e4fd1b6e88c80
Msaigwa <reggyhove@gmail.com>
2022-02-06 11:28:48 +0200

    correcting wrong imports of IntelligentNetworkService in ChargingPlatformImpl that also affected the IntegrationsConfig  and MessageConverters reference issues #4

---
+++ electronic-payments-api/src/test/java/com/econetwireless/epay/business/services/impl/PartnerCodeValidatorImplTest.java
@@ -0,0 +1,13 @@
+package com.econetwireless.epay.business.services.impl;
+
+import junit.framework.TestCase;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PartnerCodeValidatorImplTest extends TestCase {
+
+
+    @org.junit.jupiter.api.Test
+    void validatePartnerCode() {
+    }
+}
